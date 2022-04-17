797a576dad81ae437d519e8a5df82f0bc5db2ed3
Sukritan Gupta <20051871@kiit.ac.in>
2022-04-17 16:43:19 +0530

    enum

---
+++ enum/enumwithlist.py
@@ -0,0 +1,5 @@
+# here i print the second and third and fourth eleemnt of list
+a=[1,2,3,4,5,6,7,8]
+for  i ,i1 in enumerate(a):
+    if i==2 or i==3 or i==4:
+        print(i1)
---
+++ enum/syntax.py
@@ -0,0 +1,3 @@
+a=[1,2,3,4,5,6]
+for i ,item in enumerate(a):
+    print(i,item)# i gives index and item gives element
